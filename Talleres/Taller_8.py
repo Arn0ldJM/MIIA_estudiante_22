05f21d558151d80bdd4d2a5d4b1eb4b2197298ac
Arn0ldJM <89430655+Arn0ldJM@users.noreply.github.com>
2021-11-22 23:28:42 -0500

    Actualización Talleres

--- Talleres/Taller_8.py
+++ Talleres/Taller_8.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Nov 22 15:45:18 2021
 
-@author: PRESTAMO
+@author: Arnold
 """
 
 #%%
@@ -13,3 +13,11 @@ import ipywidgets as widgets
 import pandas as pd
 from ipywidgets import interactive
 
+
+df = pd.read_csv('Archivos/BankChurners.csv')
+df.head()
+
+print(df["Marital_Status"].value_counts())
+
+fig = go.Figure(data=[go.Pie(labels=[], values=[], pull=[0.2, 0, 0, 0])])
+fig.show()
